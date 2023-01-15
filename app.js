c5aa746502baf757a7326db3d7561b7d9d818dfc
Jose Luis <joseluissgz97@gmail.com>
2023-01-15 21:06:01 +0100

    ROUTER y MIDDLEWARE

--- app.js
+++ app.js
@@ -1,75 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
-import { users } from "./db.js";
+import accountRouter from "./routers/account.js";
 
 dotenv.config();
+
 const PORT = 3000;
 const expressApp = express();
+
 expressApp.use(express.json());
+expressApp.use(express.text());
+expressApp.use("/account", accountRouter);
 
-//Obetener los detalles de una cuenta del guid
-expressApp.get("/account/:guid", (req, res) => {
-  //Filtramos los usuarios que mandaremos por id mediante get
-  const { guid } = req.params;
-  const user = users.find((user) => user.guid === guid);
-
-  //Si no hay reultado mandaremos un error 404 no se encontro en contenido solicitado
-  if (!user) return res.status(404).send();
-
-  //si lo encuentra lo devolvemos
-  return res.send(user);
-});
-
-//Crear una nueva cuenta
-expressApp.post("/account", (req, res) => {
-  const { guid, name } = req.body;
-
-  if (!guid || !name) return res.state(400).send();
-
-  const user = users.find((user) => user.guid === guid);
-
-  //Si no hay reultado mandaremos un error 409 conflicto
-  if (user) return res.status(409).send();
-
-  users.push({
-    guid,
-    name,
-  });
-
-  return res.send();
-});
-
-//Actualizar el nombre de una cuenta
-expressApp.patch("/account/:guid", (req, res) => {
-  //Filtramos los usuarios que mandaremos por id mediante get
-  const { guid } = req.params;
-  const { name } = req.body;
-
-  if (!name) return res.status(400).send();
-  const user = users.find((user) => user.guid === guid);
-
-  //Si no hay reultado mandaremos un error 404 no se encontro en contenido solicitado
-  if (!user) return res.status(404).send();
-
-  user.name = name;
-
-  //si lo encuentra lo devolvemos
-  return res.send();
-});
-
-//Eliminar cuenta
-expressApp.delete("/account/:guid", (req, res) => {
-  //Eliminamos el usuarios que mandaremos por id mediante DELETE
-  const { guid } = req.params;
-  const userIndex = users.findIndex((user) => user.guid === guid);
-
-  //Si no hay reultado mandaremos un error 404 no se encontro en contenido solicitado
-  if (userIndex === -1) return res.status(404).send();
-
-  users.splice(userIndex, 1);
-
-  //devolvemos que todo a ido bien
-  return res.send();
+expressApp.get("/raiz", (req, res) => {
+  res.send();
 });
 
 expressApp.listen(PORT, () => {
---
+++ routers/account.js
@@ -0,0 +1,78 @@
+import express from "express";
+import { users } from "../db.js";
+
+//funciona de forma igual que expressApp
+const accountRouter = express.Router();
+
+//MIDDLEWARE
+accountRouter.use((req, res, next) => {
+  console.log(req.ip);
+  next();
+});
+
+//Obetener los detalles de una cuenta del guid
+accountRouter.get("/:guid", (req, res) => {
+  //Filtramos los usuarios que mandaremos por id mediante get
+  const { guid } = req.params;
+  const user = users.find((user) => user.guid === guid);
+
+  //Si no hay reultado mandaremos un error 404 no se encontro en contenido solicitado
+  if (!user) return res.status(404).send();
+
+  //si lo encuentra lo devolvemos
+  return res.send(user);
+});
+
+//Crear una nueva cuenta
+accountRouter.post("/", (req, res) => {
+  const { guid, name } = req.body;
+
+  if (!guid || !name) return res.state(400).send();
+
+  const user = users.find((user) => user.guid === guid);
+
+  //Si no hay reultado mandaremos un error 409 conflicto
+  if (user) return res.status(409).send();
+
+  users.push({
+    guid,
+    name,
+  });
+
+  return res.send();
+});
+
+//Actualizar el nombre de una cuenta
+accountRouter.patch("/:guid", (req, res) => {
+  //Filtramos los usuarios que mandaremos por id mediante get
+  const { guid } = req.params;
+  const { name } = req.body;
+
+  if (!name) return res.status(400).send();
+  const user = users.find((user) => user.guid === guid);
+
+  //Si no hay reultado mandaremos un error 404 no se encontro en contenido solicitado
+  if (!user) return res.status(404).send();
+
+  user.name = name;
+
+  //si lo encuentra lo devolvemos
+  return res.send();
+});
+
+//Eliminar cuenta
+accountRouter.delete("/:guid", (req, res) => {
+  //Eliminamos el usuarios que mandaremos por id mediante DELETE
+  const { guid } = req.params;
+  const userIndex = users.findIndex((user) => user.guid === guid);
+
+  //Si no hay reultado mandaremos un error 404 no se encontro en contenido solicitado
+  if (userIndex === -1) return res.status(404).send();
+
+  users.splice(userIndex, 1);
+
+  //devolvemos que todo a ido bien
+  return res.send();
+});
+
+export default accountRouter;
